39513babf7344e9e8f91730b2e9031a7bd35c46f
Lucas Thaynan <34990294+lucasthaynan@users.noreply.github.com>
2023-01-12 16:51:56 -0300

    update

--- main.js
+++ main.js
@@ -1,2 +1,34 @@
-let titleForm = document.querySelector('input[name="title"]')
-console.log(titleForm)
+
+// let titleForm = ''
+// let headLineForm = ''
+// let altForm = ''
+// let urlDesktopForm = ''
+// let urlMobileForm = ''
+
+let form = document.getElementById("form");
+
+
+
+function generate() {
+    
+    let titleForm = document.querySelector('input[name="title"]')
+    let headLineForm = document.querySelector('input[name="headline"]')
+    let altForm = document.querySelector('input[name="alt"]')
+    let urlDesktopForm = document.querySelector('input[name="url-desktop"]')
+    let urlMobileForm = document.querySelector('input[name="url-mobile"]')
+
+    let graphicCod = document.querySelector('.result')
+    console.log(graphicCod.innerHTML)
+
+    console.log(titleForm.value)
+    console.log(headLineForm.value)
+    console.log(altForm.value)
+    console.log(urlDesktopForm.value)
+    console.log(urlMobileForm.value)
+
+    
+}
+
+// Generate()
+
+// console.log(titleForm.value)
